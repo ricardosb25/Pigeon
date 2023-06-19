a972f7a156cc993f8fc1dfaa99d0cf4a9e795862
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-19 18:17:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Video from "./pages/Video"
+import Video from "./pages/Video";
+import db from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+
+  let maxHeight;
+  if(window.innerHeight <= 800){
+    maxHeight = window.innerHeight
+  }
+
+  const [Videos, setVideos] = useState([]);
+
+  async function getVideos() {
+    const VideosCollection = collection(db, "Videos");
+    const VideosDoc = await getDocs(VideosCollection);
+    const VideoList = VideosDoc.docs.map((doc) => doc.data());
+    setVideos(VideoList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className="AppVideos">
-        <Video
-          likes={303}
-          comments={207}
-          shares={230}
-          name="Ricardo"
-          description="Gato goleiro"
-          song="musica epica"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-        />
-        <Video
-          likes={94}
-          comments={10}
-          shares={57}
-          name="CristianoRonaldo"
-          description="Oia o Gato maluco" 
-          song="musica do gato maluco"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
-        />
+        {Videos.map((item) => {
+          return (
+            <Video
+              likes={item.likes}
+              comments={item.comments}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              song={item.song}
+              url={item.url} />
+          );
+        })}
       </div>
     </div>
   );
